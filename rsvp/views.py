63eb4aa768013f2f145e527c1f6060cf1556ba8b
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2013-10-01 21:26:47 +0100

    Create RSVP form

--- rsvp/views.py
+++ rsvp/views.py
@@ -1,8 +1,11 @@
 from braces.views import LoginRequiredMixin
 from django.views.generic import UpdateView
 
+import forms
+
 class RSVPView(LoginRequiredMixin, UpdateView):
     template_name = 'rsvp/rsvp.html' 
+    form_class = forms.RSVPForm
 
     def get_object(self):
         return self.request.user
