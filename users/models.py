87b8341e7a0cea59baff74efb0d807ab9513c0d7
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2013-10-01 23:14:29 +0100

    Improve the rsvp form and page a bit

--- users/models.py
+++ users/models.py
@@ -6,4 +6,17 @@ class WeddingUser(AbstractUser):
     Just an extension of the Django user model
     """
 
-    attending = models.NullBooleanField()
+    ATTENDING_YES = 1
+    ATTENDING_MAYBE = 0
+    ATTENDING_NO = -1
+    ATTENDING_CHOICES = (
+        (ATTENDING_YES, "Yes, I'd love to!"),
+        (ATTENDING_MAYBE, "I'm not sure yet - I'll have to get back to you"),
+        (ATTENDING_NO, "I'm afraid I can't make it"),
+    )
+
+    attending = models.IntegerField(
+        'Are you able to attend?',
+        choices = ATTENDING_CHOICES,
+        default = ATTENDING_MAYBE,
+    )
