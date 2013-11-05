07b216e98573616f2787fb32fecb44427b08db54
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2013-11-05 17:23:17 +0000

    Configure sendgrid email

--- wedding/settings/production.py
+++ wedding/settings/production.py
@@ -1,4 +1,13 @@
+import os
+
 from .base import *
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
