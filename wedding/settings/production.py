4594386c3de7d8d3bc4e4b83b045f9b9480a6f51
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2013-11-05 20:55:50 +0000

    Configure s3boto storage

--- wedding/settings/production.py
+++ wedding/settings/production.py
@@ -11,3 +11,15 @@ EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+DEFAULT_FILE_STORAGE = 'wedding.s3utils.media_root'
+STATICFILES_STORAGE = 'wedding.s3utils.static_root'
+S3_URL = 'https://{0}.s3.amazonaws.com/'.format(AWS_STORAGE_BUCKET_NAME)
+
+ADMIN_MEDIA_PREFIX = S3_URL + 'admin/'
+MEDIA_URL = S3_URL + 'client_media/'
+STATIC_URL = S3_URL + 'static_media/'
