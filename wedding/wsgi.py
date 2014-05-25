ccb1b0722c3f2077cc3552f2f38e38d8e87b47d1
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2014-05-25 10:02:53 +0100

    Configure wsgi to wrap app with Cling

--- wedding/wsgi.py
+++ wedding/wsgi.py
@@ -6,4 +6,5 @@ settings_module = "%s.settings" % PROJECT_ROOT.split(os.sep)[-1]
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+application = Cling(get_wsgi_application())
