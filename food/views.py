72a51873de554ee14a36ce477e736be9ced4dec8
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2014-05-26 13:33:45 +0100

    Set up food responding

--- food/views.py
+++ food/views.py
@@ -1,1 +1,15 @@
-# Create your views here.
+from braces.views import FormValidMessageMixin, LoginRequiredMixin
+from django.views.generic import UpdateView
+
+import forms
+from .models import FoodOffer
+
+class FoodView(LoginRequiredMixin, FormValidMessageMixin, UpdateView):
+    template_name = 'food/food.html' 
+    form_class = forms.FoodForm
+    form_valid_message = "Thanks for replying!"
+    success_url = "/"
+
+    def get_object(self):
+        obj, created = FoodOffer.objects.get_or_create(user=self.request.user)
+        return obj
