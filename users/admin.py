cfef67fc0bed03422817c4b7253e525fa39d2d7b
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2014-05-31 13:17:44 +0100

    Configure users list display

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,35 @@
+import datetime
+
 from django.contrib import admin
+from django.utils import timezone
 from models import WeddingUser
 
-admin.site.register(WeddingUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'long_name',
+        'email',
+        'has_logged_in',
+        'last_login',
+        'get_rsvp_date',
+        'get_rsvp',
+    )
+
+    def get_rsvp_date(self, obj):
+        return obj.rsvp_date or ''
+    get_rsvp_date.short_description = 'RSVP date'
+
+    def get_rsvp(self, obj):
+        return self.get_rsvp_date(obj) and obj.get_attending_display()
+    get_rsvp.short_description = 'RSVP'
+
+    def has_logged_in(self, obj):
+        date = obj.last_login
+        if not date:
+            return False
+        return date > timezone.make_aware(
+            datetime.datetime(2014,1,1),
+            timezone.get_default_timezone()
+        )
+    has_logged_in.boolean = True
+
+admin.site.register(WeddingUser, UserAdmin)
